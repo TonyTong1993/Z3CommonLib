9fcaa3396c27955ab034fe1808893d0a84e91f77
Tony Tony <tongwanhua1993@163.com>
2019-10-08 19:33:11 +0800

    更新宿迁的项目结构

--- Example/Z3CommonLib/Z3Common.h
+++ Example/Z3CommonLib/Z3Common.h
@@ -8,12 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-//日期格式
-extern NSString * const Z3CommonDateFormat0;
-extern NSString * const Z3CommonDateFormat1;
 
-//最近一次上传位置的时间
-extern NSString * const Z3CommonLastUploadLocationsDateKey;
-extern NSString * const Z3CommonLastUploadLocationsCountKey;
+
 
 
--- Example/Z3CommonLib/Z3Common.m
+++ Example/Z3CommonLib/Z3Common.m
@@ -6,8 +6,6 @@
 //  Copyright © 2019 TonyTong1993. All rights reserved.
 //
 
-NSString * const Z3CommonDateFormat0 = @"yyyy-MM-dd HH:mm:ss";
-NSString * const Z3CommonDateFormat1 = @"yyyy-MM-dd";
 
-NSString * const Z3CommonLastUploadLocationsDateKey = @"upload.location.date.key";
-NSString * const Z3CommonLastUploadLocationsCountKey = @"upload.location.count.key";
+
+
